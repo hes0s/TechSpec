c9f9439756649028b9149b6fd47fad9447acaa7b
Cusnir Cristian <grecikinmr@gmail.com>
2026-05-15 20:38:17 +0300

    forgot

---
+++ src/app/components/shared/footer/footer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-general-footer',
+  imports: [],
+  templateUrl: './footer.html',
+  styleUrl: './footer.css',
+})
+export class GeneralFooter {
+
+}
