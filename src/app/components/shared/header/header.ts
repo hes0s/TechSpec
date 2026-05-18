b559b1f762441132fe28fd96329d9469d1bc6792
hes0s <grecikinmr@gmail.com>
2026-05-18 17:00:27 +0300

    ui changes before adding filters

--- src/app/components/shared/header/header.ts
+++ src/app/components/shared/header/header.ts
@@ -8,43 +8,50 @@ import { ThemeService } from '../../../services/theme';
   selector: 'app-general-header',
   imports: [RouterLink],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrl: './header.css',
 })
 export class GeneralHeader {
   constructor(public theme: ThemeService) {}
-  private localS = inject(LocalStorageService)
-  private auth = inject(AuthService)
-  loggedIn : boolean = false
-  cartCount: number = 0
+  private localS = inject(LocalStorageService);
+  private auth = inject(AuthService);
+  loggedIn: boolean = false;
+  isAdmin: boolean = false;
+  cartCount: number = 0;
 
-
-  getCartStatus(){
-    this.cartCount = this.localS.getItemCountCart() 
+  getCartStatus() {
+    this.cartCount = this.localS.getItemCountCart();
   }
 
-  watchCartUpdate(){
-    this.localS.cart.subscribe(cart => {
-      this.cartCount = cart.reduce((total, item) => total + item.quantity, 0)
-    })
+  watchCartUpdate() {
+    this.localS.cart.subscribe((cart) => {
+      this.cartCount = cart.reduce((total, item) => total + item.quantity, 0);
+    });
   }
 
-  getStatus(){
+  getStatus() {
     this.auth.getStatus().subscribe((user) => {
-      if(user){
+      if (user) {
         this.loggedIn = true;
+        if (user != null && user.email == 'cusnircristi161@gmail.com' && user != null) {
+          console.log('Admin access granted');
+          this.isAdmin = true;
+        } else {
+          alert('You are not an admin');
+        }
       } else {
         this.loggedIn = false;
-      }})
+      }
+    });
   }
 
-  logOut(){
-    this.auth.logOut()
-    alert("logged out")
+  logOut() {
+    this.auth.logOut();
+    alert('logged out');
   }
 
   ngOnInit() {
-    this.getStatus()
-    this.watchCartUpdate()
-    this.getCartStatus()
+    this.getStatus();
+    this.watchCartUpdate();
+    this.getCartStatus();
   }
 }
